5589fe0bce87dc52579a9e52aa6aace1b3a41759
Tinh Nguyen <tinh.nguyen.trung@tpssoft.com>
2023-04-16 15:17:41 +0700

    update scripts

--- scripts/android_build.sh
+++ scripts/android_build.sh
@@ -1,11 +1,11 @@
 if [ $3 == "bundle" ]
 then
 flutter build appbundle --build-name=$1 --build-number=$2
-firebase appdistribution:distribute build/app/outputs/bundle/release/app-release.aab  --app 1:544862340537:android:c2bb35873359795a2a2268
-open build/app/outputs/bundle/release
+firebase appdistribution:distribute build/app/outputs/bundle/release/app-release.aab  --app 1:544862340537:android:c2bb35873359795a2a2268 --groups "tester"
+# open build/app/outputs/bundle/release
 else
 flutter build apk --build-name=$1 --build-number=$2
-firebase appdistribution:distribute build/app/outputs/flutter-apk/app-release.apk  --app 1:544862340537:android:c2bb35873359795a2a2268
-open build/app/outputs/flutter-apk
+firebase appdistribution:distribute build/app/outputs/flutter-apk/app-release.apk  --app 1:544862340537:android:c2bb35873359795a2a2268 --groups "tester"
+# open build/app/outputs/flutter-apk
 fi
 
